639b9fb5cc49fa022c7868b3c2d62303f0a71278
annleeli <annlee.li@gmail.com>
2016-03-06 06:24:09 +0000

    some stylings + bg image

--- src/app.js
+++ src/app.js
@@ -20,73 +20,63 @@ myApp.controller('ctrl', ['$scope', '$timeout', "$q", "$http" ,function ($scope,
     $scope.political = {};
     $scope.keywords = "";
     
-    var i =0;
+    $scope.searched = false;
     
     function indico(method) {
         return 'https://apiv2.indico.io/' + method + '?key=78f0b703f174e46a4a0afca0c782c263'
     }
-    
-    function getSentiment () {
-        var deferred = $q.defer();
-        $.post(
-          indico("sentiment"),
-          JSON.stringify({
-            'data': $scope.searchText
-          })
-        ).then(function(res) { 
-            
-            $scope.$apply(function(){
-                deferred.resolve(res)
-            })
-            
-            return deferred.promise;
-        })
-    }
-    
+
     $scope.submitSearch = function() {
-    
-        $.post(
-          indico("sentiment"),
-          JSON.stringify({
-            'data': $scope.searchText
-          })
-        ).then(function(res) {
-            res = JSON.parse(res);
-            console.log("sentiment",res) 
-            $scope.sentiment = res;
-        }).then(function() {
-            $scope.$apply();
-        });
         
+        if ($scope.searchText) {
+            $scope.searched = true;
         
-        $.post(
-          indico("political"),
-          JSON.stringify({
-            'data': $scope.searchText
-          })
-        ).then(function(res) {
-            res = JSON.parse(res);
-            console.log("political",res) 
-            $scope.political = res;
-        }).then(function() {
-            $scope.$apply();
-        });
+            $.post(
+              indico("sentiment"),
+              JSON.stringify({
+                'data': $scope.searchText
+              })
+            ).then(function(res) {
+                res = JSON.parse(res);
+                console.log("sentiment",res) 
+                $scope.sentiment = res;
+            }).then(function() {
+                $scope.$apply();
+            });
+            
+            
+            $.post(
+              indico("political"),
+              JSON.stringify({
+                'data': $scope.searchText
+              })
+            ).then(function(res) {
+                res = JSON.parse(res);
+                console.log("political",res) 
+                $scope.political = res;
+            }).then(function() {
+                $scope.$apply();
+            });
+            
+            $.post(
+              indico("keywords"),
+              JSON.stringify({
+                'data': $scope.searchText
+              })
+            ).then(function(res) {
+                res = JSON.parse(res);
+                console.log("keywords",res) 
+                $scope.keywords = res;
+            }).then(function() {
+                $scope.$apply();
+            });
+            
+            
         
-        $.post(
-          indico("keywords"),
-          JSON.stringify({
-            'data': $scope.searchText
-          })
-        ).then(function(res) {
-            res = JSON.parse(res);
-            console.log("keywords",res) 
-            $scope.keywords = res;
-        }).then(function() {
-            $scope.$apply();
-        });
-        
-        
-        
+            console.log($scope.searched)
+        } else {
+            $scope.searched = false;
+        }
         
         // // batch example
         // $.post(
